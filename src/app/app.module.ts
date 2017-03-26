ff6dec57cf7838e05c1c057dd3f9b74a5eb8791f
grit0 <krit405f@gmail.com>
2017-03-26 15:15:06 +0700

    fix node_module + import angularfire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { AngularFireModule } from 'angularfire2';
 @NgModule({
   declarations: [
     MyApp,
